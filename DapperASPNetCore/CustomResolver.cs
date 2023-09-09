9f41e8ef0b9e5ef362315e6e494ee9d4a520032c
Manish <ManishManjrekar@outlook.com>
2023-09-09 16:44:56 +0530

    first check for adding a project

---
+++ DapperASPNetCore/CustomResolver.cs
@@ -0,0 +1,19 @@
+﻿using Dapper;
+using System.Reflection;
+using System;
+
+namespace DapperASPNetCore
+{
+    public class CustomResolver : SimpleCRUD.ITableNameResolver, SimpleCRUD.IColumnNameResolver
+    {
+        public string ResolveTableName(Type type)
+        {
+            return string.Format("tbl_{0}", type.Name);
+        }
+
+        public string ResolveColumnName(PropertyInfo propertyInfo)
+        {
+            return string.Format("{0}_{1}", propertyInfo.DeclaringType.Name, propertyInfo.Name);
+        }
+    }
+}
